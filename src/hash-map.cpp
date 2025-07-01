b0db97fdb8a3a7ae9a4fd6078e541f8bf01ee428
Joao Marcos <joao.marcos.witcel@gmail.com>
2025-06-30 22:11:51 -0300

    WIP: implementando PUT

---
+++ src/hash-map.cpp
@@ -0,0 +1,72 @@
+#pragma once
+
+#include <assert.h>
+#include <stdint.h>
+#include <string.h>
+
+size_t hash(const char* value)
+{
+  // @todo João, implementar uma função razoável
+  return strlen(value);
+}
+
+template <typename Value_Type>
+struct Hash_Table_item {
+  const char* key;
+  Value_Type value;
+  Hash_Table_item* next_item;
+};
+
+template <typename Value_Type>
+struct Hash_Map {
+
+  size_t capacity;
+  size_t occupancy;
+
+  Hash_Table_item<Value_Type>* bucket;
+
+  Hash_Map(size_t capacity)
+  {
+    this->capacity = capacity;
+    this->bucket = new Hash_Table_item<Value_Type>[capacity];
+  }
+
+  bool put(const char* key, Value_Type value)
+  {
+    size_t index = hash(key) % this->capacity;
+
+    Hash_Table_item<Value_Type>* item = &this->bucket[index];
+
+    for (; item; item = item->next_item)
+    {
+      if (item->key == NULL)
+      {
+        item->key = key;
+        item->value = value;
+        this->occupancy++;
+        
+        return true;
+      }
+      else if (strcmp(item->key, key) == 0)
+      {
+        item->key = key;
+        item->value = value;
+        
+        return true;
+      }
+      else if (item->next_item == NULL)
+      {
+        Hash_Table_item<Value_Type>* new_item = new Hash_Table_item<Value_Type>;
+        new_item->key = key;
+        new_item->value = value;
+
+        item->next_item = new_item;
+        this->occupancy++;
+        
+        return true;
+      }
+    }
+    
+    return false;
+  }
+};
